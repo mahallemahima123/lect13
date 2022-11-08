a83c900e713e581e2677bba5ef694dcc288b1e40
SantoshTurerao <santosh.turerao@infinx.com>
2022-11-08 21:45:52 +0530

    updated all

---
+++ JS/conversion.js
@@ -0,0 +1,15 @@
+// false =0 , true =1
+//null 0
+
+//  Addition concanate 
+m=true+true+true;
+console.log(m)
+
+let num1='10'
+let num2='20'
+
+
+
+// let total=Number(num1)+Number(num2);
+
+console.log(Boolean(1))
