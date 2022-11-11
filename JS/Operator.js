38621f60be17c6aa91f4f46ae3000e9fb47d87c1
SantoshTurerao <santosh.turerao@infinx.com>
2022-11-11 20:41:54 +0530

    session 6 n 7

---
+++ JS/Operator.js
@@ -0,0 +1,60 @@
+
+// let k=10;
+// let m=++k;
+// console.log(m)
+
+// Binary 
+   // Arithmatic Operator 
+
+    // let a,b,c;  //declaration 
+
+    // a=30;
+    // b=20;
+    // c=a/b;
+    // console.log(c)
+
+
+    // //Assigment 
+    //     //1. =  : 
+
+    //        let k,j;
+    //        k=10;
+    //        j=10;
+
+    //     //    k=k+j;
+    //        k*=j;
+    //        console.log(k)
+
+
+    // Relational 
+    
+    
+
+
+    // let mark=45;
+
+    // let pass=mark>=45;
+    // console.log(pass);
+
+
+    // Equality 
+
+    //1.==
+
+//     let mark=true;
+
+//    console.log(1==mark);
+
+let userName='santosh';
+let password='santosh';
+
+let login= userName=='santosh' && password=='santosh';
+
+userId ='s@gmail.com'
+mobile='996712000'
+
+login =   userId== 's@gmail.com' || mobile=='456789';
+
+console.log(login);
+
+console.log(login)
---
+++ p1.js
@@ -0,0 +1,10 @@
+
+
+
+
+let fruits=["Apple","Mango","Banana"];
+
+for(let i=0;i<3;i++){
+    document.write("<br>"+fruits[i]);
+}
+let days=["Sun","Mon","Tue","Thu","Fri","SAT","SUN"]
